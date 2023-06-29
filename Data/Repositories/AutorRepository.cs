1e7cd6269b93b5b46505f6e0cf6f09a9c03c5dd5
Vanessa Rocha <87250768+NessaRocha@users.noreply.github.com>
2023-06-29 09:41:03 -0300

    .

--- Data/Repositories/AutorRepository.cs
+++ Data/Repositories/AutorRepository.cs
@@ -21,30 +21,30 @@ namespace GerenciamentoDeBiblioteca.Data.Repositories
 
         public Autor GetAutorById(int id)
         {
-            return _context.Autor.Find(id);
+            return _context.Set<Autor>().Find(id);
         }
 
         public void AddAutor(Autor autor)
         {
-            _context.Autor.Add(autor);
+            _context.Set<Autor>().Add(autor);
             _context.SaveChanges();
         }
 
         public void UpdateAutor(Autor autor)
         {
-            _context.Autor.Update(autor);
+            _context.Set<Autor>().Update(autor);
             _context.SaveChanges();
         }
 
         public void DeleteAutor(Autor autor)
         {
-            _context.Autor.Remove(autor);
+            _context.Set<Autor>().Remove(autor);
             _context.SaveChanges();
         }
 
         public IList<Autor> GetAllAutor()
         {
-
+            throw new NotImplementedException();
         }
     }
 }
